63871422ebf7d3b5b47b80155b250db5cf5ca9f4
Bugi Hermansyah <bugihermansyah@gmail.com>
2024-06-30 17:15:32 +0700

    tampilkan data outstanding berdasarkan role

--- app/Filament/Resources/OutstandingResource/Pages/ListOutstandings.php
+++ app/Filament/Resources/OutstandingResource/Pages/ListOutstandings.php
@@ -5,10 +5,27 @@ namespace App\Filament\Resources\OutstandingResource\Pages;
 use App\Filament\Resources\OutstandingResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class ListOutstandings extends ListRecords
 {
     protected static string $resource = OutstandingResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        $user = Auth::user();
+
+        if ($user->hasRole(['head', 'staff'])) {
+            return parent::getTableQuery()
+                ->join('locations', 'outstandings.location_id', '=', 'locations.id')
+                ->join('teams', 'locations.team_id', '=', 'teams.id')
+                ->where('teams.id', $user->team_id)
+                ->select('outstandings.*');
+        }
+
+        return parent::getTableQuery();
+    }
 
     protected function getHeaderActions(): array
     {
