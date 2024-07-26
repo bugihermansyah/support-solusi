060ed4fdd721c5a0f9c29ce0fca64ac90bddba09
Bugi Hermansyah <bugihermansyah@gmail.com>
2024-07-26 23:49:43 +0700

    wip

--- app/Models/Reporting.php
+++ app/Models/Reporting.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 use App\Enums\ReportStatus;
+use Guava\Calendar\Contracts\Eventable;
+use Guava\Calendar\ValueObjects\Event;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Reporting extends Model implements HasMedia
+class Reporting extends Model implements HasMedia, Eventable
 {
     use HasFactory;
     use HasUlids;
@@ -27,12 +30,20 @@ class Reporting extends Model implements HasMedia
         'date_visit',
         'user_id',
         'status',
+        'revisit',
         'note'
     ];
 
     protected $casts = [
         'status' => ReportStatus::class,
     ];
+
+    public function toEvent(): Event|array {
+        return Event::make($this)
+            ->title($this->cause)
+            ->start($this->date_visit)
+            ->end($this->date_visit);
+    }
 
     /**
      * Get the user that owns the Reporting
@@ -41,7 +52,7 @@ class Reporting extends Model implements HasMedia
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     /**
@@ -52,5 +63,30 @@ class Reporting extends Model implements HasMedia
     public function outstanding(): BelongsTo
     {
         return $this->belongsTo(Outstanding::class);
+    }
+
+    /**
+     * Get all of the outstandingunits for the Outstanding
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function outstandingunits(): HasMany
+    {
+        return $this->hasMany(OutstandingUnit::class, 'outstanding_id', 'outstanding_id');
+    }
+
+    public function getTitle(): string
+    {
+        return $this->status;
+    }
+
+    public function getStart(): string
+    {
+        return $this->date_visit;
+    }
+
+    public function getEnd(): string
+    {
+        return $this->date_visit;
     }
 }
