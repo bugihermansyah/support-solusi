bbf317c59afacc68474000328b2302d3bfa5ac99
Bugi Hermansyah <bugihermansyah@gmail.com>
2024-10-02 00:59:41 +0700

    add panel monitoring and widget

---
+++ app/Filament/Monitoring/Widgets/AdvancedStatsOverviewWidget.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Monitoring\Widgets;
+
+use Carbon\Carbon;
+use EightyNine\FilamentAdvancedWidget\AdvancedStatsOverviewWidget as BaseWidget;
+use Filament\Facades\Filament;
+use EightyNine\FilamentAdvancedWidget\AdvancedStatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\DB;
+
+class AdvancedStatsOverviewWidget extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $today = Carbon::now()->toDateString();
+
+        $openOutstandings = DB::table('outstandings')->count();
+
+        $implementationLocations = DB::table('locations')
+            ->where('status', 'imple')
+            ->count();
+
+        $dailyTarget = DB::table('reportings')
+            ->where('date_visit', $today);
+
+        // Get the total count of reportings for today
+        $totalCount = $dailyTarget->count();
+
+        // Get the count of reportings with a non-null status
+        $dailyProgress = $dailyTarget->whereNotNull('status')->count();
+
+        // Calculate the percentage only if totalCount is greater than 0
+        if ($totalCount > 0) {
+            $percentage = ($dailyProgress / $totalCount) * 100;
+        } else {
+            $percentage = 0; // Or set it to another default value as needed
+        }
+
+        return [
+            Stat::make('Open Outstandings', $openOutstandings)->icon('heroicon-o-fire')
+                ->progress(100)
+                ->progressBarColor('danger')
+                ->chartColor('success')
+                ->iconPosition('start')
+                ->description('The users in this period')
+                ->descriptionIcon('heroicon-o-information-circle', 'before')
+                ->descriptionColor('success')
+                ->iconColor('danger'),
+            Stat::make('Daily Progress', $totalCount)->icon('heroicon-o-calendar-days')
+                ->progress(round($percentage, 2))
+                ->progressBarColor('success')
+                ->iconPosition('start')
+                ->description("The comments in this period")
+                ->descriptionIcon('heroicon-o-chevron-down', 'before')
+                ->descriptionColor('success')
+                ->iconColor('danger'),
+            Stat::make('Implementations', $implementationLocations)->icon('heroicon-o-home')
+                ->progress(100)
+                ->progressBarColor('success')
+                ->iconPosition('start')
+                ->description('Location in implementation status')
+                ->descriptionIcon('heroicon-o-paper-airplane', 'before')
+                ->descriptionColor('primary')
+                ->iconColor('warning'),
+            Stat::make('Daily Progress', $totalCount)->icon('heroicon-o-calendar-days')
+                ->progress(round($percentage, 2))
+                ->progressBarColor('success')
+                ->iconPosition('start')
+                ->description("The comments in this period")
+                ->descriptionIcon('heroicon-o-chevron-down', 'before')
+                ->descriptionColor('success')
+                ->iconColor('danger'),
+            Stat::make('Daily Progress', $totalCount)->icon('heroicon-o-calendar-days')
+                ->progress(round($percentage, 2))
+                ->progressBarColor('success')
+                ->iconPosition('start')
+                ->description("The comments in this period")
+                ->descriptionIcon('heroicon-o-chevron-down', 'before')
+                ->descriptionColor('success')
+                ->iconColor('danger'),
+        ];
+    }
+
+    public static function canView(): bool
+    {
+        // Tampilkan hanya di panel 'monitoring'
+        return Filament::getCurrentPanel() === Filament::getPanel('monitoring');
+    }
+
+    protected function getColumns(): int
+    {
+        return 3;
+    }
+}
---
+++ app/Providers/Filament/MonitoringPanelProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use App\Filament\Monitoring\Widgets\AdvancedStatsOverviewWidget;
+use App\Filament\Monitoring\Widgets\DailyChartWidget;
+use Filament\Enums\ThemeMode;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class MonitoringPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('monitoring')
+            ->path('monitoring')
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->navigation(false)
+            ->maxContentWidth(MaxWidth::Full)
+            ->defaultThemeMode(ThemeMode::Dark)
+            ->font('Poppins')
+            ->brandName('Support Monitoring')
+            ->discoverResources(in: app_path('Filament/Monitoring/Resources'), for: 'App\\Filament\\Monitoring\\Resources')
+            ->discoverPages(in: app_path('Filament/Monitoring/Pages'), for: 'App\\Filament\\Monitoring\\Pages')
+            ->pages([
+                // Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Monitoring/Widgets'), for: 'App\\Filament\\Monitoring\\Widgets')
+            ->widgets([
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+                AdvancedStatsOverviewWidget::class,
+                DailyChartWidget::class
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                // Authenticate::class,
+            ]);
+    }
+}
