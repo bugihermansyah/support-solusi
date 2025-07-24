56ca0bb039e10a6fe1308e5ba1f493c892a24cd0
Bugi Hermansyah <bugihermansyah@gmail.com>
2025-07-24 11:45:22 +0700

    refactor DiskUsageResource to optimize Eloquent query and update table columns

--- app/Filament/Resources/DiskUsageResource.php
+++ app/Filament/Resources/DiskUsageResource.php
@@ -9,15 +9,29 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\DB;
 
 class DiskUsageResource extends Resource
 {
     protected static ?string $model = DiskUsage::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getEloquentQuery(): Builder
+{
+    // Ambil id terbaru (created_at paling akhir) untuk tiap location_id
+    $latestIds = DB::table('disk_usages')
+        ->select(DB::raw('MAX(id) as id'))
+        ->groupBy('location_id');
+
+    return parent::getEloquentQuery()
+        ->whereIn('id', $latestIds)
+        ->whereHas('location');
+}
 
     public static function form(Form $form): Form
     {
@@ -31,18 +45,26 @@ class DiskUsageResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('location.name')->label('Location'),
+                TextColumn::make('mount_point'),
+                TextColumn::make('size'),
+                TextColumn::make('used'),
+                TextColumn::make('available'),
+                TextColumn::make('usage_percent'),
+                TextColumn::make('created_at')
+                    ->label('Last Update')
+                    ->dateTime(),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 
@@ -57,8 +79,8 @@ class DiskUsageResource extends Resource
     {
         return [
             'index' => Pages\ListDiskUsages::route('/'),
-            'create' => Pages\CreateDiskUsage::route('/create'),
-            'edit' => Pages\EditDiskUsage::route('/{record}/edit'),
+            // 'create' => Pages\CreateDiskUsage::route('/create'),
+            // 'edit' => Pages\EditDiskUsage::route('/{record}/edit'),
         ];
     }
 }
--- app/Filament/Resources/DiskUsageResource/Pages/ListDiskUsages.php
+++ app/Filament/Resources/DiskUsageResource/Pages/ListDiskUsages.php
@@ -13,7 +13,7 @@ class ListDiskUsages extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
