6fadce4982ea982d6c73b5cd5ccbcc40b46ae8ac
Bugi Hermansyah <bugihermansyah@gmail.com>
2025-12-28 21:04:30 +0700

    refactor: remove unused methods and comments from TypeContract enum

--- app/Enums/TypeContract.php
+++ app/Enums/TypeContract.php
@@ -2,8 +2,6 @@
 
 namespace App\Enums;
 
-// use Filament\Support\Contracts\HasColor;
-// use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
 
 enum TypeContract: string implements HasLabel
@@ -20,24 +18,4 @@ enum TypeContract: string implements HasLabel
             self::Kredit => 'Kredit',
         };
     }
-
-    // public function getColor(): string | array | null
-    // {
-    //     return match ($this) {
-    //         self::Implementasi => 'warning',
-    //         self::New => 'info',
-    //         self::Cancelled => 'danger',
-    //     };
-    // }
-
-    // public function getIcon(): ?string
-    // {
-    //     return match ($this) {
-    //         self::New => 'heroicon-m-sparkles',
-    //         self::Processing => 'heroicon-m-arrow-path',
-    //         self::Shipped => 'heroicon-m-truck',
-    //         self::Delivered => 'heroicon-m-check-badge',
-    //         self::Cancelled => 'heroicon-m-x-circle',
-    //     };
-    // }
 }
